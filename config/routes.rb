bfe6b702d096db6e05926e58561b618c3ec4e5f5
Petro Lesapiti <petrolesapiti@gmail.com>
2023-12-05 12:02:25 +0300

    Update gems and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  # get 'categories/index'
-  # get 'categories/show'
-  # get 'categories/new'
-  # get 'categories/delete'
-  # get 'categories/edit'
   resources :tasks do
     member do
       get :delete
     end
   end
-  root 'main#index'
+  root 'tasks#index'
   # get 'main/index'
   # get 'tasks/index'
   # get 'tasks/new'
@@ -21,7 +16,7 @@ Rails.application.routes.draw do
       get :delete
     end 
   end 
-  match 'about', to: 'main#about', via: :get
+  match 'about', to: 'tasks#about', via: :get
   # Defines the root path route ("/")
   # root "articles#index"
 end
